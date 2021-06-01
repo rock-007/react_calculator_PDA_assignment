38ba6e42014605ae5f535a3c8b3ca14e226a2997
rock-007 <umair.ashraf2019@gmail.com>
2021-06-01 16:04:06 +0100

    Integration test seperated and orginised

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -3,8 +3,36 @@ describe("Calculator", () => {
     beforeEach(() => {
         cy.visit("http://localhost:3000");
     });
-
+    //Do the number buttons update the display of the running total?
     it("should have working number buttons", () => {
+        cy.get("#number2").click();
+        cy.get(".display").should("contain", "2");
+        cy.get("#operator_add").click();
+        cy.get("#number3").click();
+        cy.get("#operator_equals").click();
+        cy.get(".display").should("contain", "5");
+    });
+
+    //Do the arithmetical operations update the display with the result of the operation?
+    it("should have working arithmetical operations ", () => {
+        cy.get("#number2").click();
+        cy.get(".display").should("contain", "2");
+        cy.get("#operator_add").click();
+        cy.get("#number3").click();
+        cy.get("#operator_equals").click();
+        cy.get(".display").should("contain", "5");
+    });
+    //Can multiple operations be chained together?
+    it("should have working multiple operations", () => {
+        cy.get("#number2").click();
+        cy.get(".display").should("contain", "2");
+        cy.get("#operator_multiply").click();
+        cy.get("#number3").click();
+        cy.get("#operator_equals").click();
+        cy.get(".display").should("contain", "6");
+    });
+    //Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+    it("should have working multiple operations", () => {
         cy.get("#number2").click();
         cy.get(".display").should("contain", "2");
         cy.get("#operator_add").click();
@@ -21,6 +49,10 @@ describe("Calculator", () => {
         cy.get("#number5").click();
         cy.get("#operator_equals").click();
         cy.get(".display").should("contain", "-1.5");
+    });
+    //What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
+    it("should have working multiple operations", () => {
+        cy.get("#number2").click();
         cy.get("#operator_divide").click();
         cy.get("#number0").click();
         cy.get("#operator_equals").click();
