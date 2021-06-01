d777b5bb37e8b5699deebc3392489bf6408f1494
rock-007 <umair.ashraf2019@gmail.com>
2021-06-01 16:19:49 +0100

    add range of numbers

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -32,7 +32,7 @@ describe("Calculator", () => {
         cy.get(".display").should("contain", "6");
     });
     //Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
-    it("should have working multiple operations", () => {
+    it("should have working for range of numbers", () => {
         cy.get("#number2").click();
         cy.get(".display").should("contain", "2");
         cy.get("#operator_add").click();
@@ -49,9 +49,24 @@ describe("Calculator", () => {
         cy.get("#number5").click();
         cy.get("#operator_equals").click();
         cy.get(".display").should("contain", "-1.5");
+        cy.get("#operator_multiply").click();
+        cy.get("#number2").click();
+        cy.get("#number2").click();
+        cy.get("#number2").click();
+        cy.get("#number2").click();
+        cy.get("#number2").click();
+        cy.get("#number2").click();
+        cy.get("#number2").click();
+        cy.get("#number2").click();
+        cy.get("#number2").click();
+        cy.get("#operator_equals").click();
+        cy.get(".display").should("contain", "-333333333");
+
+
+
     });
     //What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
-    it("should have working multiple operations", () => {
+    it("should have working in exceptional circumstances", () => {
         cy.get("#number2").click();
         cy.get("#operator_divide").click();
         cy.get("#number0").click();
